8600848ab5870431f8bc2a64c60aeb46bda7b77f
BryanBroos <bryanbroos764@gmail.com>
2023-10-27 16:41:02 -0600

    Preview de la practica

---
+++ firma.js
@@ -0,0 +1,5 @@
+const cavas = document.querySelector('canvas');
+const form = document.querySelector('.firma-pad-form');
+const botonLimpiar = document.querySelector('.boton-limpiar');
+const botonImagen = document.querySelector('.boton-imagen');
+const botonContrato = document.querySelector('.boton.contrato');
